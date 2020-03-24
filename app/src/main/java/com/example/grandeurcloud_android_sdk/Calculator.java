5f3b92423424b2306ac6ad4d055b45933d092133
thedesicoder <talha.sameed@gmail.com>
2020-03-24 23:30:51 +0500

    A sample library of calculator has been created and tested in another project. Build.gradle of App has been changed. Application id has been removed and apply plugin has been changed to library.

---
+++ app/src/main/java/com/example/grandeurcloud_android_sdk/Calculator.java
@@ -0,0 +1,28 @@
+package com.example.grandeurcloud_android_sdk;
+
+// A simple calculator class which adds,
+// subtracts, divides and multiplies
+// two number.
+public class Calculator {
+
+//  Function to add two numbers
+    public int add(int a,int b){
+        return a+b;
+    }
+
+//  Function to subtract two numbers
+    public int sub(int a,int b){
+        return a-b;
+    }
+
+//  Function to subtract two numbers
+    public int div(int a,int b){
+        return a/b;
+    }
+
+//  Function to multiply two numbers
+    public int mul(int a,int b){
+        return a*b;
+    }
+}
+
